18871ed2cf866828158d40b401b37b2639e72221
vikbert <segentor@gmail.com>
2021-04-05 20:19:19 +0100

    update init members

--- src/services/store.js
+++ src/services/store.js
@@ -2,15 +2,19 @@ import {writable} from 'svelte/store';
 import * as memo from './memo';
 
 const dummyNames = [
-    {name: 'Bert Sandoval', checked: true},
-    {name: 'Tami Castillo', checked: true},
-    {name: 'Tonny Simmons', checked: true},
-    {name: 'Dale Petterson', checked: true},
-    {name: 'Roy Armstrong', checked: true},
+    {name: 'Alexander', checked: true},
+    {name: 'Paul', checked: true},
+    {name: 'Lukas', checked: true},
+    {name: 'Sophia', checked: true},
+    {name: 'Louis', checked: true},
+    {name: 'Ludwig', checked: true},
+    {name: 'Maximilian', checked: true},
+    {name: 'Johanna', checked: true},
+    {name: 'Maria', checked: true},
 ];
 
 // storage keys
-const KEY_MEMBERS = 'wal_memmbers';
+const KEY_MEMBERS = 'wal_members';
 const KEY_CONFIG = 'wal_config';
 const KEY_PROJECT = 'wal_project';
 
@@ -18,7 +22,7 @@ const KEY_PROJECT = 'wal_project';
 const initItems = memo.get(KEY_MEMBERS) ?? dummyNames;
 
 // opened: if the setting popup is opened
-export const opened = writable(false);
+export const opened = writable(true);
 
 // paused: if the timer is paused
 export const paused = writable(true);
