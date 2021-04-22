ba7fc14f88102b2da4c26b6af7faa50815b3d635
vikbert <segentor@gmail.com>
2021-04-22 12:09:09 +0200

    update the other components

---
+++ src/components/AudioSwitch.svelte
@@ -0,0 +1,22 @@
+<script>
+    import Dock from '../Dock.svelte'
+    export let enabled = true;
+</script>
+
+
+{#if enabled}
+<Dock>
+    <span class="iconify" data-icon="ant-design:audio-outlined" data-inline="false"/>
+</Dock>
+{:else}
+<Dock>
+    <span class="iconify" data-icon="ant-design:audio-muted-outlined" data-inline="false"></span>
+</Dock>
+{/if}
+
+<style>
+    .iconify {
+        color: red;
+        font-size: 40px;
+    }
+</style>
