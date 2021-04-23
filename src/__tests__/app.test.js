8e09ed87816ae56656792316d3c832d831f9aa3d
vikbert <segentor@gmail.com>
2021-04-23 10:01:20 +0200

    updates

---
+++ src/__tests__/app.test.js
@@ -0,0 +1,5 @@
+describe('your empty test', () => {
+  it('should return true', () => {
+    expect(true).toBe(true);
+  });
+})
--- src/__tests__/app.test.ts
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/svelte";
-import App from "../App.svelte";
-
-test("should render the text hello world!", () => {
-  const results = render(App);
-
-  console.log(results);
-
-});
